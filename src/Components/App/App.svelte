e6b999460f23bfc66d865a124d6b00b2c4af2ecb
Dmitry <dmitry@cojam.ru>
2020-08-23 09:37:50 +0500

    fixes

---
+++ src/Components/App/App.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import { Router, Link, Route } from 'svelte-routing';
+
+    import IndexPage from '../IndexPage/IndexPage.svelte';
+</script>
+
+<style lang="scss">
+
+</style>
+
+<Router>
+    <Route path="/">
+        <IndexPage />
+    </Route>
+</Router>
